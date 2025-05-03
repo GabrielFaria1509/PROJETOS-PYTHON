482b3a7cda911680f628dc38329dfb136ef10ef8
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-05-02 22:49:27 -0300

    Update ControlWebcam.py

--- ControlWebcam.py
+++ ControlWebcam.py
@@ -1,6 +1,10 @@
 import cv2 as cv   ##puxa biblioteca OpenCV
 
 webcam = cv.VideoCapture(0)    ## 0 = webcam padrão do computador
+## mostrar cobrindo tela toda 
+webcam.set(cv.CAP_PROP_FRAME_WIDTH, 1920)  ## largura da imagem
+webcam.set(cv.CAP_PROP_FRAME_HEIGHT, 720)  ## altura da imagem
+
 
 if webcam.isOpened() :
    validacao , frame = webcam.read()  ## lê a imagem da webcam
@@ -10,7 +14,7 @@ if webcam.isOpened() :
        key = cv.waitKey(10)        ## espera 10ms para ler a tecla pressionada
        if key == 27:
            break   ## se a tecla pressionada for ESC, sai do loop
-cv.imwrite("imagem.jpg", frame)  ## salva a imagem capturada 
+cv.imwrite("PrintWebcam.jpg", frame)  ## salva a imagem capturada 
 
 webcam.release()   ## libera a webcam
 cv.destroyAllWindows()  ## fecha todas as janelas abertas
