c0b3159ef54217f9e17244f48b492c1aceeb6dba
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-05-03 15:19:43 -0300

    Update ControlWebcam.py

--- ControlWebcam.py
+++ ControlWebcam.py
@@ -6,29 +6,32 @@ webcam = cv.VideoCapture(0)    ## 0 = webcam padrão do computador
 webcam.set(cv.CAP_PROP_FRAME_WIDTH, 1920)  ## largura da imagem
 webcam.set(cv.CAP_PROP_FRAME_HEIGHT, 720)  ## altura da imagem
 
-webcam.set(cv.CAP_PROP_FPS, 30)  ## frames por segundo
+fps = webcam.set(cv.CAP_PROP_FPS, 30)  ## frames por segundo
 
+if webcam.isOpened():
+    validacao, frame = webcam.read()  ## lê a imagem da webcam
+    while validacao:  ## enquanto a webcam estiver aberta
+        validacao, frame = webcam.read()
 
-if webcam.isOpened() :
-   validacao , frame = webcam.read()  ## lê a imagem da webcam
-   while validacao:         ## enquanto a webcam estiver aberta
-       validacao, frame = webcam.read()
-       
-       data = dt.now()  ## pega a data e hora atual
-       exibicao = data.strftime("%d-%m-%Y %H:%M:%S")  ## formata a data e hora para o formato desejado
+        data = dt.now()  ## pega a data e hora atual
+        exibicao = data.strftime("%d-%m-%Y %H:%M:%S")  ## formata a data e hora
 
-       cv.putText(frame , exibicao,(50,100),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)## mostra a data e hora na tela
-       cv.putText(frame, "Pressione ESC para sair",(50,150),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)## mostra a mensagem na tela
-       cv.putText(frame, "Sorria !",(50,50),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)## mostra a mensagem na tela
-       
-       cv.imshow("Webcam", frame)          ## mostra a imagem da webcam
-       key = cv.waitKey(10)        ## espera 10ms para ler a tecla pressionada
-       
-       if key == 27:
-           break   ## se a tecla pressionada for ESC, sai do loop
-cv.imwrite("PrintWebcam.jpg", frame)  ## salva a imagem capturada 
+        # Mensagens ajustadas
+        #primeiro parenteses é a posição do texto(x,y)
+        #primero num isolada é a escala , segundo é a espessura
+        #parenteses com 3 núemeros é a cor (B,G,R) ,RGB o quanto tem de azul verde e vermelho
+        cv.putText(frame, "Sorria!", (30, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv.putText(frame, "Pressione ESC para sair", (30, 90), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv.putText(frame, exibicao, (30, 130), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv.putText(frame, f"Frame por segundo: {int(webcam.get(cv.CAP_PROP_FPS))}", (30, 170),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
+        cv.imshow("Webcam", frame)  ## mostra a imagem da webcam
+        key = cv.waitKey(10)  ## espera 10ms para ler a tecla pressionada
 
-webcam.release()   ## libera a webcam
+        if key == 27:
+            break  ## se a tecla pressionada for ESC, sai do loop
+
+cv.imwrite("PrintWebcam.jpg", frame)  ## salva a imagem capturada
+webcam.release()  ## libera a webcam
 cv.destroyAllWindows()  ## fecha todas as janelas abertas
-       
