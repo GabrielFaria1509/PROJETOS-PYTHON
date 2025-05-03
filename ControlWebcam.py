15e78717b019c61ca62d93cbc8bc6aacf8d74a69
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-05-03 13:59:41 -0300

    Update ControlWebcam.py

--- ControlWebcam.py
+++ ControlWebcam.py
@@ -10,11 +10,18 @@ if webcam.isOpened() :
    validacao , frame = webcam.read()  ## lê a imagem da webcam
    while validacao:         ## enquanto a webcam estiver aberta
        validacao, frame = webcam.read()
+       
+       cv.putText(frame, "Sorria !",(50,50),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)## mostra a mensagem na tela
+       
        cv.imshow("Webcam", frame)          ## mostra a imagem da webcam
        key = cv.waitKey(10)        ## espera 10ms para ler a tecla pressionada
+       
+       cv.putText(frame, "Registro salvo",(50,50),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)## mostra a mensagem na tela
+       
        if key == 27:
            break   ## se a tecla pressionada for ESC, sai do loop
 cv.imwrite("PrintWebcam.jpg", frame)  ## salva a imagem capturada 
+
 
 webcam.release()   ## libera a webcam
 cv.destroyAllWindows()  ## fecha todas as janelas abertas
